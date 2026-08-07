36ca18a6245aaa63536be8cb15347a25902c5c5d
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-07 09:24:28 +0700

    Change Helmet import to react-helmet-async

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import React, { Suspense } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
