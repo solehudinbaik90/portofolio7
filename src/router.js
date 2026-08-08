61b5688d81ade9a47d781279bf0725157f1b5243
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-08 07:24:39 +0700

    Integrate ErrorBoundary for route error handling
    Added ErrorBoundary component for error handling in routes.

--- src/router.js
+++ src/router.js
@@ -7,14 +7,19 @@ import Home from "./pages/Home";
 import Notfound from "./pages/Notfound";
 import Portfolios from "./pages/Portfolios";
 import Resumes from "./pages/Resumes";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/resume", element: <Resumes /> },
-  { path: "/portfolios", element: <Portfolios /> },
-  { path: "/blogs", element: <Blogs /> },
-  { path: "/blogs/:id/:title", element: <BlogDetails /> },
-  { path: "/contact", element: <Contact /> },
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorBoundary />,
+  },
+  { path: "/about", element: <About />, errorElement: <ErrorBoundary /> },
+  { path: "/resume", element: <Resumes />, errorElement: <ErrorBoundary /> },
+  { path: "/portfolios", element: <Portfolios />, errorElement: <ErrorBoundary /> },
+  { path: "/blogs", element: <Blogs />, errorElement: <ErrorBoundary /> },
+  { path: "/blogs/:id/:title", element: <BlogDetails />, errorElement: <ErrorBoundary /> },
+  { path: "/contact", element: <Contact />, errorElement: <ErrorBoundary /> },
   { path: "*", element: <Notfound /> },
 ]);
